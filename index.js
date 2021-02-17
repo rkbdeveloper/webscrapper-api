2c1053804fbe3411b2724fe30a14d9f000fa531c
Renz Kenneth Busarang <renzbusarang@gmail.com>
2021-02-17 14:39:27 +0800

    Update routing and link functions

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3131
-const screenshot = require('./links')
+const {GetScreenShot, GetFacebookData} = require('./links')
 
 app.get('/', (req, res) => res.status(200).json({ status: 'ok' }))
 
 app.get('/screenshot', (req, res) => {
   const url = req.query.url
-  ;(async () => {
-    const buffer = await screenshot(url)
+  (async () => {
+    const buffer = await GetScreenShot(url)
     res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
     res.setHeader('Content-Type', 'image/png')
+    res.send(buffer)
+  })()
+})
+
+app.post('/facebook', (req, res) => {
+  const url = req.query.url
+  (async () => {
+    const buffer = await GetFacebookImages(url)
+    res.setHeader('Content-Type', 'application/json')
     res.send(buffer)
   })()
 })
--- links.js
+++ links.js
@@ -1,20 +1,32 @@
 const puppeteer = require('puppeteer')
 
-module.exports = function (url) {
-  return new Promise((resolve, reject) => {
-    ;(async () => {
+module.exports = {
+  GetScreenShot : screenShot,
+  GetFacebookData : getFacebookData,
+}
 
+function getChromeOptions(){
     const chromeOptions = {
-        headless: true,
-        defaultViewport: null,
-        args: [
-            "--incognito",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote"
-        ],
+      headless: true,
+      defaultViewport: null,
+      args: [
+          "--incognito",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote"
+      ],
     };
-      const browser = await puppeteer.launch(chromeOptions);
+
+    return chromeOptions;
+}
+
+// param1 string url 
+function screenShot(url){
+  return new Promise((resolve, reject) => {
+    (async () => {
+
+    
+      const browser = await puppeteer.launch(getChromeOptions);
 
       const page = await browser.newPage()
 
@@ -22,9 +34,73 @@ module.exports = function (url) {
         waitUntil: ['load', 'networkidle0', 'domcontentloaded']
       })
 
-      await page.waitFor(1000)
+      await page.waitForTimeout(1000)
 
-      await page.emulateMedia('screen')
+      const buffer = await page.screenshot({
+        fullPage: true,
+        type: 'png'
+      })
+
+      await browser.close()
+
+      resolve(buffer)
+    })()
+  })
+}
+
+
+function getFacebookData(url){
+
+}
+
+function getSnapchatData(url){
+
+}
+
+function getMegaData(url){
+
+}
+
+
+// param1 string url 
+function getImages(url){
+  return new Promise((resolve, reject) => {
+    (async () => {
+
+      const browser = await puppeteer.launch(getChromeOptions);
+
+      const page = await browser.newPage()
+
+      await page.goto(url, {
+        waitUntil: ['load', 'networkidle0', 'domcontentloaded']
+      })
+
+      await page.waitForTimeout(1000)
+
+      const buffer = await page.screenshot({
+        fullPage: true,
+        type: 'png'
+      })
+
+      await browser.close()
+
+      resolve(buffer)
+    })()
+  })
+}
+
+function getVidoes(url){
+  return new Promise((resolve, reject) => {
+    (async () => {
+      const browser = await puppeteer.launch(getChromeOptions);
+
+      const page = await browser.newPage()
+
+      await page.goto(url, {
+        waitUntil: ['load', 'networkidle0', 'domcontentloaded']
+      })
+
+      await page.waitForTimeout(1000)
 
       const buffer = await page.screenshot({
         fullPage: true,
