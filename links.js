4d4061fa2fbefbd435993ee8737fdedf5aa5de15
Renz Kenneth Busarang <renzbusarang@gmail.com>
2021-02-17 18:40:39 +0800

    Fix for sandbox problem

--- links.js
+++ links.js
@@ -11,6 +11,8 @@ function getChromeOptions(){
       args: [
           "--no-sandbox",
           '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--single-process'
       ],
     };
 
