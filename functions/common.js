30add021efd8c5918b9f355853507ed7a7e832fd
Renz Kenneth Busarang <renzbusarang@gmail.com>
2021-02-26 01:41:11 +0800

    instagram scraping update

--- functions/common.js
+++ functions/common.js
@@ -28,7 +28,8 @@ function GetImageLinks(url, regex){
         (async () => {
           const browser = await puppeteer.launch(getChromeOptions());
           const page = await browser.newPage()
-    
+          await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
+          
           await page.goto(url, {
             waitUntil: ['load', 'networkidle0', 'domcontentloaded']
           })
